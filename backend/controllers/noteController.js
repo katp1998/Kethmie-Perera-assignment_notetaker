c2f974efed0b271ce2213cab045c4f177f187594
Kethmie Perera <47343068+katp1998@users.noreply.github.com>
2022-07-06 19:44:52 +0530

    REST API for notes
    Signed-off-by: Kethmie Perera <47343068+katp1998@users.noreply.github.com>

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -4,7 +4,8 @@ const Note = require("../models/noteModel");
 //This is to get notes
 //@route GET /api/goals @access PRIVATE
 const getNotes = asyncHandler(async (rq, rs) => {
-  rs.status(200).json({ message: "get note!" });
+  const notes = await Note.find();
+  rs.status(200).json(notes);
 });
 
 //This is to set notes
@@ -14,19 +15,35 @@ const setNotes = asyncHandler(async (rq, rs) => {
     rs.status(400);
     throw new Error("please add a text field!");
   }
-  rs.status(200).json({ message: "create note!" });
+  const note = await Note.create({
+    text: rq.body.text,
+    title: rq.body.text,
+  });
+
+  rs.status(200).json(note);
 });
 
 //This is to update notes
 //@route PUT /api/goals @access PRIVATE
 const updateNotes = asyncHandler(async (rq, rs) => {
-  rs.status(200).json({ message: `update note ${rq.params.id}!` });
+  const note = await Note.findById(rq.params.id);
+
+  //if the id is not set
+  if (!note) {
+    rs.status(400);
+    throw new Error("note not found");
+  }
+
+  const updatedNote = await Note.findById(rq.params.id, rq.body, { new: true });
+
+  rs.status(200).json(updatedNote);
 });
 
 //This is to delete notes
 //@route DELETE /api/goals @access PRIVATE
 const deleteNotes = asyncHandler(async (rq, rs) => {
-  rs.status(200).json({ message: `delete note ${rq.params.id}!` });
+  await Note.remove();
+  rs.status(200).json({ id: rq.params.id });
 });
 
 module.exports = {
