ceb446a9127a8bc667080af1d0ceede753b323fb
Kethmie Perera <47343068+katp1998@users.noreply.github.com>
2022-07-11 11:22:48 +0530

    updating readme
    Signed-off-by: Kethmie Perera <47343068+katp1998@users.noreply.github.com>

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -68,7 +68,7 @@ const registerUser = asyncHandler(async (rq, rs) => {
 //This is to authenticate users - POST @route: /api/users/login @access: PUBLIC
 const loginUser = asyncHandler(async (rq, rs) => {
   const { email, password } = rq.body;
-  const user = await User.findOne({ email }); // --- cant make email addresses that are similar eg:
+  const user = await User.findOne({ email }); // --- cant make email addresses that are similar eg: perera.kethmie. perera.dilakshi
   if (user && (await bcrypt.compare(password, user.password))) {
     rs.json({
       message: "logged in successfully!",
